ae3ff681e40905cd1a4252c2921bff7ba57cff0e
Kevin Ho <bornarkun@gmail.com>
2018-10-27 21:26:18 -0400

    two screens

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,147 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React, { Component } from 'react';
+import { Text, TextInput, View, StyleSheet, Button } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 40,
+    marginLeft: 50,
+    marginRight: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+
+  container2: {
+    justifyContent: 'center',
+    marginTop: 20,
+    marginLeft: 50,
+    marginRight: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+
+  container3: {
+    justifyContent: 'center',
+    marginTop: 20,
+    marginLeft: 50,
+    marginRight: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+
+  container4: {
+    justifyContent: 'center',
+    marginTop: 40,
+    marginLeft: 80,
+    marginRight: 80,
+    padding: 20,
+    backgroundColor: '#ffffff',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+
+  containerButton: {
+    justifyContent: 'center',
+    marginTop: 40,
+    marginLeft: 80,
+    marginRight: 80,
+    padding: 20,
+    backgroundColor: 'rgb(66, 191, 244)',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+
+  buttonText: {
+    color: 'rgb(0, 0, 0)',
+    fontSize: 30,
+  },
+
+  paragraph: {
+    margin: 12,
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+
+  header: {
+    marginTop: 40,
+    fontSize: 28,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'rgb(66, 191, 244)',
+  },
+});
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'BuddyUp!',
   };
 
+  constructor() {
+    super();
+    this.state = {
+      name: null,
+      major: null,
+      year: null,
+      hometown: null,
+    };
+  }
+
+  submitForm() {}
+
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View>
+        <View>
+          <Text style={styles.header}>New User Form</Text>
+        </View>
+
+        <View style={styles.container}>
+          <TextInput
+            style={styles.paragraph}
+            placeholder="Name"
+            onChangeText={text => this.setState({ name: text })}
+          />
+        </View>
+
+        <View style={styles.container2}>
+          <TextInput
+            style={styles.paragraph}
+            placeholder="Major"
+            onChangeText={text => this.setState({ major: text })}
+          />
+        </View>
+
+        <View style={styles.container3}>
+          <TextInput
+            style={styles.paragraph}
+            placeholder="Year"
+            onChangeText={text => this.setState({ year: text })}
+          />
+        </View>
+
+        <View style={styles.container3}>
+          <TextInput
+            style={styles.paragraph}
+            placeholder="Hometown"
+            onChangeText={text => this.setState({ hometown: text })}
+          />
+        </View>
+        <View style={styles.containerButton}>
+          <Button
+            title="Match me!"
+            onPress={this.submitForm}
+            color="rgb(256,256,256)"
+          />
+        </View>
+      </View>
+    );
   }
 }
